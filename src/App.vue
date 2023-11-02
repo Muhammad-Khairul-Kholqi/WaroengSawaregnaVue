211101e6d84de83d86ad52e374ebf2495f038b2e
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2023-11-02 21:33:12 +0700

    Project sederhana menggunakan vue js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Layouts/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
